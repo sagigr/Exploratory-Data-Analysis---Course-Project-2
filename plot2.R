5c14a19d401ce0b6205faa652ec538bbba6e2b45
Sagi Greenstine <sagigr@gmail.com>
2015-07-15 14:59:38 +0300

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -11,7 +11,7 @@ baltimore<-subset(NEI, NEI$fips==24510)
 ##Summing emissions per year
 totalBaltimore<-tapply(baltimore$Emissions, INDEX=baltimore$year, sum)
 ## Creating the plot 2
-barplot(totalBaltimore, main="Total Emissions in Baltimore, MD by Year", xlab="Year", ylab="Emissions")
+barplot(totalBaltimore, main=expression('Total PM'[2.5]*' Emissions in Baltimore, MD by Year'), xlab="Year", ylab="Emissions")
 ## Saving to file
 dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
