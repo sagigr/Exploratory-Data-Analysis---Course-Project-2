4dd8ecfdfd401c31a2fb38ab43fdbceceef9ca5b
Sagi Greenstine <sagigr@gmail.com>
2015-07-15 23:04:07 +0300

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -12,7 +12,7 @@ coalRelated <- grepl("coal", SCC$SCC.Level.Four, ignore.case=TRUE)
 coalCombustion <- (combustionRelated & coalRelated)
 combustionSCC <- SCC[coalCombustion,]$SCC
 combustionNEI <- NEI[NEI$SCC %in% combustionSCC,]
-
+## Creating the png file
 png("plot4.png",width=480,height=480,units="px",bg="transparent")
 
 library(ggplot2)
