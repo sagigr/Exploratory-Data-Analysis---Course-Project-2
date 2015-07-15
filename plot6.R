5c166f93e018cb2d3d22b3a6e41b558c2d7cc51f
Sagi Greenstine <sagigr@gmail.com>
2015-07-15 23:45:41 +0300

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -6,14 +6,20 @@ if(!exists("NEI")){
 if(!exists("SCC")){
   SCC <- readRDS("./Source_Classification_Code.rds")
 }
-
-library(ggplot2)
-
+##
 sources <- SCC[grepl("On-Road", SCC$EI.Sector),]
 sources <- sources$SCC
 data <- NEI[NEI$SCC %in% sources,]
 data <- NEI[NEI$fips %in% c("24510", "06037"),]
 data <- aggregate(Emissions ~ year + fips, data=data, sum)
+## Creating the png file
+png("plot6.png",width=10,height=5,units="px",bg="transparent")
+## Creating the plot
+library(ggplot2)
+ggpl <- qplot(year, Emissions, color=fips, data=data, geom="path" +
+main="Emissions From Vehicle Related Sources in Baltimore and Los Angeles County", xlab="Year", ylab="Emissions")
+#ggsave(ggpl, file="plot6.png", width=10, height=5)
 
-ggpl <- qplot(year, Emissions, color=fips, data=data, geom="path", main="Emissions From Vehicle Related Sources in Baltimore and Los Angeles County", xlab="Year", ylab="Emissions")
-ggsave(ggpl, file="plot6.png", width=10, height=5)
+print(ggpl)
+
+dev.off()
