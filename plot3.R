62613d10caeaf4bdbd6a4514a8c76930a2bcf3c2
Sagi Greenstine <sagigr@gmail.com>
2015-07-15 22:56:21 +0300

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -7,21 +7,21 @@ if(!exists("SCC")){
   SCC <- readRDS("./Source_Classification_Code.rds")
 }
 ## Subsetting NEI data by Baltimore's fip
-baltimore<-subset(NEI, NEI$fips==24510)
-# Aggregating using sum the Baltimore emissions data by year
+baltimoreNEI<-subset(NEI, NEI$fips==24510)
+## Aggregating using sum the Baltimore emissions data by year
 aggTotalsBaltimore <- aggregate(Emissions ~ year, baltimoreNEI,sum)
 
 png("plot3.png",width=480,height=480,units="px",bg="transparent")
 
 library(ggplot2)
 
-ggp <- ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+ggpl <- ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
   geom_bar(stat="identity") +
   theme_bw() + guides(fill=FALSE)+
   facet_grid(.~type,scales = "free",space="free") + 
-  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
-  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+  labs(x="Year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City, MD 1999-2008 by Source Type"))
 
-print(ggp)
+print(ggpl)
 
 dev.off()
