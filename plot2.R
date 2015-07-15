5470e01020054e8defbf57561a8b7eb626f569be
Sagi Greenstine <sagigr@gmail.com>
2015-07-15 14:51:09 +0300

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -6,3 +6,11 @@ if(!exists("NEI")){
 if(!exists("SCC")){
   SCC <- readRDS("./Source_Classification_Code.rds")
 }
+## Subsetting NEI data by Baltimore's fip
+baltimore<-subset(NEI, NEI$fips==24510)
+##Summing emissions per year
+totalBaltimore<-tapply(baltimore$Emissions, INDEX=baltimore$year, sum)
+## Creating the plot 2
+barplot(totalBaltimore, main="Total Emissions in Baltimore, MD by Year", xlab="Year", ylab="Emissions")
+## Saving to file
+
