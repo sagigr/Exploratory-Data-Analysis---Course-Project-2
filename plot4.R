5eb5e58912d884b05313063f4cb516c470a0e45f
Sagi Greenstine <sagigr@gmail.com>
2015-07-15 16:30:58 +0300

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,8 @@
+## Reading the data
+## This first line will likely take a few seconds. Be patient!
+if(!exists("NEI")){
+  NEI <- readRDS("./summarySCC_PM25.rds")
+}
+if(!exists("SCC")){
+  SCC <- readRDS("./Source_Classification_Code.rds")
+}
