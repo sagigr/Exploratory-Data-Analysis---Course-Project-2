aa4c8871c6ce1d425d0c60b46d4f73e95b673b70
Sagi Greenstine <sagigr@gmail.com>
2015-07-15 23:05:55 +0300

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -14,15 +14,14 @@ combustionSCC <- SCC[coalCombustion,]$SCC
 combustionNEI <- NEI[NEI$SCC %in% combustionSCC,]
 ## Creating the png file
 png("plot4.png",width=480,height=480,units="px",bg="transparent")
-
+## Creating the plot
 library(ggplot2)
-
-ggp <- ggplot(combustionNEI,aes(factor(year),Emissions/10^5)) +
+ggpl <- ggplot(combustionNEI,aes(factor(year),Emissions/10^5)) +
   geom_bar(stat="identity",fill="grey",width=0.75) +
   theme_bw() +  guides(fill=FALSE) +
-  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
+  labs(x="Year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
   labs(title=expression("PM"[2.5]*" Coal Combustion Source Emissions Across US from 1999-2008"))
 
-print(ggp)
+print(ggpl)
 
 dev.off()
