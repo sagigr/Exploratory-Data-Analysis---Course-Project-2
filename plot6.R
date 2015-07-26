f8c31546fafbc04c60c66465f16134cd54c2d008
Sagi Greenstine <sagigr@gmail.com>
2015-07-26 17:43:46 +0300

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -9,16 +9,17 @@ if(!exists("SCC")){
 ## Subsetting the data by Baltimore City (BC) and Los Angeles County (LAC) emissions from motor vehicle sources
 BCEmissions <- subset(NEI, fips == 24510 & type == 'ON-ROAD')
 LACEmissions <- subset(NEI, fips == 06037 & type == 'ON-ROAD')
-# Aggregates
-MD.DF <- aggregate(MD.onroad[, 'Emissions'], by = list(MD.onroad$year), sum)
-colnames(MD.DF) <- c('year', 'Emissions')
-MD.DF$City <- paste(rep('MD', 4))
+## Aggregating the Baltimore City (BC) and Los Angeles County (LAC) emissions from motor vehicle sources
+AggBCEmissions <- aggregate(BCEmissions[, 'Emissions'], by = list(BCEmissions$year), FUN=sum)
+colnames(AggBCEmissions) <- c('Year', 'Emissions')
+AggBCEmissions$City <- paste(rep('Baltimore City', 4))
+AggLACEmissions <- aggregate(LACEmissions[, 'Emissions'], by = list(LACEmissions$year), FUN=sum)
+colnames(AggLACEmissions) <- c('Year', 'Emissions')
+AggLACEmissions$City <- paste(rep('Los Angeles County', 4))
+BCLAC <- as.data.frame(rbind(AggBCEmissions, AggLACEmissions))
 
-CA.DF <- aggregate(CA.onroad[, 'Emissions'], by = list(CA.onroad$year), sum)
-colnames(CA.DF) <- c('year', 'Emissions')
-CA.DF$City <- paste(rep('CA', 4))
 
-DF <- as.data.frame(rbind(MD.DF, CA.DF))
+
 
 png('plot6.png')
 ggplot(data = DF, aes(x = year, y = Emissions)) + geom_bar(aes(fill = year),stat = "identity") + guides(fill = F) + ggtitle('Total Emissions of Motor Vehicle Sources\nLos Angeles County, California vs. Baltimore City, Maryland') + ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position = 'none') + facet_grid(. ~ City) + geom_text(aes(label = round(Emissions, 0), size = 1, hjust = 0.5, vjust = -1))
