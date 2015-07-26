ed8f95486ba9e161b6d9476c6e072c907b2391da
Sagi Greenstine <sagigr@gmail.com>
2015-07-26 18:07:02 +0300

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -17,10 +17,24 @@ AggLACEmissions <- aggregate(LACEmissions[, 'Emissions'], by = list(LACEmissions
 colnames(AggLACEmissions) <- c('Year', 'Emissions')
 AggLACEmissions$City <- paste(rep('Los Angeles County', 4))
 BCLAC <- as.data.frame(rbind(AggBCEmissions, AggLACEmissions))
-
-
-
-
-png('plot6.png')
-ggplot(data = DF, aes(x = year, y = Emissions)) + geom_bar(aes(fill = year),stat = "identity") + guides(fill = F) + ggtitle('Total Emissions of Motor Vehicle Sources\nLos Angeles County, California vs. Baltimore City, Maryland') + ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position = 'none') + facet_grid(. ~ City) + geom_text(aes(label = round(Emissions, 0), size = 1, hjust = 0.5, vjust = -1))
+## Creating the png file
+png("plot6.png",width=640,height=520)
+## Creating the Plot 6
+library(ggplot2)
+BCLACEmissions <- ggplot(BCLAC, aes(factor(year), Emissions)) + geom_bar(aes(fill = year), stat="identity") +
+theme(legend.position = 'none') + guides(fill=FALSE)+ facet_grid(. ~ City) + 
+geom_text(aes(label = round(Emissions, 0), size = 1, hjust = 0.5, vjust = -1)) + labs(x="Year", y="Emissions (Tons)") +
+labs(title="Emissions from Motor Vehicle Sources in Baltimore City vs. Los Angeles County")
+print(BCLACEmissions)
 dev.off()
+
+#ggplot(data = DF, aes(x = year, y = Emissions)) + 
+#geom_bar(aes(fill = year),stat = "identity") + 
+#guides(fill = F) + 
+#ggtitle('Total Emissions of Motor Vehicle Sources\nLos Angeles County, California vs. Baltimore City, Maryland') + 
+#ylab(expression('PM'[2.5])) + 
+#xlab('Year') + 
+#theme(legend.position = 'none') + 
+#facet_grid(. ~ City) + 
+#geom_text(aes(label = round(Emissions, 0), size = 1, hjust = 0.5, vjust = -1))
+#dev.off()
