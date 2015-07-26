cf07b9bb938528fb8f9ae73273663ea389d91356
Sagi Greenstine <sagigr@gmail.com>
2015-07-26 14:48:21 +0300

    Update plot5.R

--- plot5.R
+++ plot5.R
@@ -6,23 +6,7 @@ if(!exists("NEI")){
 if(!exists("SCC")){
         SCC <- readRDS("./Source_Classification_Code.rds")
 }
-## Gathering the subset of the NEI data which corresponds to vehicles
-vehicles <- grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
-vehiclesSCC <- SCC[vehicles,]$SCC
-vehiclesNEI <- NEI[NEI$SCC %in% vehiclesSCC,]
-
-## Subsetting the vehicles NEI data to Baltimore's fip
-baltimoreVehiclesNEI <- vehiclesNEI[vehiclesNEI$fips=="24510",]
-## Creating the png file
-png("plot5.png",width=480,height=480)
-## Creating the plot 5
-library(ggplot2)
-ggpl <- ggplot(baltimoreVehiclesNEI,aes(factor(year),Emissions)) +
-  geom_bar(stat="identity",fill="grey",width=0.75) +
-  theme_bw() +  guides(fill=FALSE) +
-  labs(x="Year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
-  labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore from 1999-2008"))
-
-print(ggpl)
+## Subsetting the data by Baltimore City (BC) emissions from motor vehicle sources
+BCEmissions <-subset(NEI, fips==24510 & type=="ON-ROAD")
 
 dev.off()
