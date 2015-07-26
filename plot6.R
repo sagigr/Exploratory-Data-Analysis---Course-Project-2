efcfcb1461c4d9af93e904140ad1f73d101f83d3
Sagi Greenstine <sagigr@gmail.com>
2015-07-26 17:26:09 +0300

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -7,30 +7,20 @@ if(!exists("SCC")){
         SCC <- readRDS("./Source_Classification_Code.rds")
 }
 ## Subsetting the data by Baltimore City (BC) and Los Angeles County (LAC) emissions from motor vehicle sources
+BCEmissions <- subset(NEI, fips == 24510 & type == 'ON-ROAD')
+CA.onroad <- subset(NEI, fips == 06037 & type == 'ON-ROAD')
 
-data <- subset(NEI, type == 'ON-ROAD')
-data$year <- factor(data$year, levels=c('1999', '2002', '2005', '2008'))
+# Aggregates
+MD.DF <- aggregate(MD.onroad[, 'Emissions'], by = list(MD.onroad$year), sum)
+colnames(MD.DF) <- c('year', 'Emissions')
+MD.DF$City <- paste(rep('MD', 4))
 
-MD <- subset(data, fips == '24510')
-CA <- subset(data, fips == '06037')
+CA.DF <- aggregate(CA.onroad[, 'Emissions'], by = list(CA.onroad$year), sum)
+colnames(CA.DF) <- c('year', 'Emissions')
+CA.DF$City <- paste(rep('CA', 4))
 
-MD <- aggregate(MD[, 'Emissions'], by=list(MD$year), sum)
-colnames(MD) <- c('year', 'Emissions')
-MD$City <- paste(rep('MD', 4))
-
-CA <- aggregate(CA[, 'Emissions'], by=list(CA$year), sum)
-colnames(CA) <- c('year', 'Emissions')
-CA$City <- paste(rep('CA', 4))
-
-data <- as.data.frame(rbind(MD, CA))
+DF <- as.data.frame(rbind(MD.DF, CA.DF))
 
 png('plot6.png')
-
-require(ggplot2)
-
-abc <- ggplot(data=data, aes(x=year, y=Emissions)) + geom_bar(aes(fill=year), stat="identity") + guides(fill=F) + 
-  ggtitle('Total Emissions of Motor Vehicle Sources\nLos Angeles County, California vs. Baltimore City, Maryland') + 
-  ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position='none') + facet_grid(. ~ City) + 
-  geom_text(aes(label=round(Emissions,0), size=1, hjust=0.5, vjust=-1))
-print(abc)
+ggplot(data = DF, aes(x = year, y = Emissions)) + geom_bar(aes(fill = year),stat = "identity") + guides(fill = F) + ggtitle('Total Emissions of Motor Vehicle Sources\nLos Angeles County, California vs. Baltimore City, Maryland') + ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position = 'none') + facet_grid(. ~ City) + geom_text(aes(label = round(Emissions, 0), size = 1, hjust = 0.5, vjust = -1))
 dev.off()
