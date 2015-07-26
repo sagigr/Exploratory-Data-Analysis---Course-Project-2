91899b384ec08cc727822301547345f0ca50a55f
Sagi Greenstine <sagigr@gmail.com>
2015-07-26 13:24:47 +0300

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,10 +1,10 @@
 ## Reading the data
 ## This first line will likely take a few seconds. Be patient!
 if(!exists("NEI")){
-        NEI <- readRDS("./summarySCC_PM25.rds")
+  NEI <- readRDS("./summarySCC_PM25.rds")
 }
 if(!exists("SCC")){
-        SCC <- readRDS("./Source_Classification_Code.rds")
+  SCC <- readRDS("./Source_Classification_Code.rds")
 }
 ## Finding coal combustion-related sources (CCRS)
 isCCRS <- grepl("Fuel Comb.*Coal", SCC$EI.Sector)
@@ -14,11 +14,11 @@ CCRSE <- NEI[(NEI$SCC %in% CCRS$SCC), ]
 ## Groupping by years
 CCRSEY <- aggregate(Emissions ~ year, data=CCRSE, FUN=sum)
 ## Creating the png file
-png("plot4.png",width=600,height=480)
+png("plot4.png",width=640,height=520)
 ## Creating the Plot 4
 library(ggplot2)
-CoalEmissions <- ggplot(CCRSEY/10^3, aes(x=factor(year), y=Emissions)) + 
-geom_bar(stat="identity") + xlab("Year") + ylab("Emissions (kTons)") + 
-ggtitle("Emissions from Coal Combustion-Related Sources Across the United States, 1999–2008")
+CoalEmissions <- ggplot(CCRSEY/10^3, aes(factor(year), Emissions)) + geom_bar(stat="identity") + theme_bw() + 
+guides(fill=FALSE)+ labs(x="Year", y="Emissions (kTons)") + 
+labs(title="Emissions from Coal Combustion-Related Sources Across the United States, 1999–2008")
 print(CoalEmissions)
 dev.off()
